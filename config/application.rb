3a925ebe72d800818141d64b568910c6971f95ed
lionelderrick97 <debatonon@gmail.com>
2019-09-13 12:40:02 +0200

    “Application”

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module MyApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.i18n.available_locales= [:en, :fr]
 	config.i18n.default_locale = :fr
 	config.active_storage.service = :local
     # Settings in config/environments/* take precedence over those specified here.
